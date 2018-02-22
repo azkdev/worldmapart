79da806b47724caf17c85d1baeb84949fcc10204
azkdev <azkdev@outlook.com>
2018-02-22 21:18:41 +0200

    PApplet implemented.

--- src/main/kotlin/com/azkdev/worldmapart/WorldMapArt.kt
+++ src/main/kotlin/com/azkdev/worldmapart/WorldMapArt.kt
@@ -1,4 +1,23 @@
 package com.azkdev.worldmapart
 
-class WorldMapArt {
+import processing.core.PApplet
+
+class WorldMapArt: PApplet() {
+
+    override fun settings() {
+
+    }
+
+    override fun setup() {
+
+    }
+
+    override fun draw() {
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+    PApplet.main("com.azkdev.worldmapart.WorldMapArt")
 }
